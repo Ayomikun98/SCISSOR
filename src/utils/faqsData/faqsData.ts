3b4172fc921f3897dc95041b3df060169ae54182
Ayomikun98 <jamesayantunji98@gmail.com>
2024-08-15 20:37:36 +0100

    Initial commit with project files

---
+++ src/utils/faqsData/faqsData.ts
@@ -0,0 +1,38 @@
+export const faqsData = [
+  {
+    que: 'How does URL shortening work?',
+    ans: 'URL shortening takes your lengthy web address and whittles it down to a sleek, snappy link. Click it, and you are whisked away to the original destination—just a lot faster',
+  },
+  {
+    que: 'Is it necessary to create an account to use the URL shortening service?',
+    ans: 'No need to sign up for anything! You can start shortening links right away. Create an account if you want extra perks like custom links and tracking.',
+  },
+  {
+    que: 'Are the shortened links permanent? Will they expire?',
+    ans: 'Our shortened links are here for the long haul—no expiration dates. Once they are shortened, they stay that way',
+  },
+  {
+    que: 'Are there any limitations on the number of URLs I can shorten?',
+    ans: 'Shorten as many URLs as you like! There is no cap on how many links you can make sleek and manageable',
+  },
+  {
+    que: 'Can I customize the shortened URLs to reflect my brand or content?',
+    ans: 'Absolutely! You can craft custom short links that match your brand or message, adding a personal touch to every URL',
+  },
+  {
+    que: 'Can I track the performance of my shortened URLs?',
+    ans: 'Yes, you can! Track how your links are performing with our analytics—see clicks, locations, and more',
+  },
+  {
+    que: 'How secure is the URL shortening service? Are the shortened links protected against spam or malicious activity?',
+    ans: 'We haveve got security covered! Our shortened links are protected against spam and malicious activity to keep your browsing safe',
+  },
+  {
+    que: 'What is a QR code and what can it do?',
+    ans: 'URL shortening works by taking a long URL and creating a shorter, condensed version that redirects to the original URL. When a user clicks on the shortened link, they are redirected to the intended destination.',
+  },
+  {
+    que: 'Is there an API available for integrating the URL shortening service into my own applications or websites?',
+    ans: 'URL shortening works by taking a long URL and creating a shorter, condensed version that redirects to the original URL. When a user clicks on the shortened link, they are redirected to the intended destination.',
+  },
+];
